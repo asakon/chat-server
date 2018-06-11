7389faaf01b5af49b42005bf09ac06e45aa94bbf
asakon <kondoasako@gmail.com>
2018-06-11 10:59:13 +0900

    expressインスタンス化

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,9 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
+const express = require('express');
+const app = express();
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //

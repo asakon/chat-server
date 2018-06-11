60ea7a4c39d383ce9ac8ecfa5583f3c14619fc0f
asakon <kondoasako@gmail.com>
2018-06-11 11:28:42 +0900

    ユーザー情報取得

--- functions/index.js
+++ functions/index.js
@@ -8,9 +8,30 @@ const app = express();
 const cors = require('cors')({origin: true});
 app.use(cors);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const anonymousUser = {
+  id: "anon",
+  name: "Anonymous",
+  avatar: ""
+};
+
+const checkUser = (req, res, next) => {
+  req.user = anonymousUser;
+  if (req.query.auth_token != undefined) {
+    let idToken = req.query.auth_token;
+    admin.auth().verifyIdToken(idToken).then(decodedIdToken => {
+      let autoUser = {
+        id: decodedIdToken.user_id,
+        name: decodedIdToken.name,
+        avatar: decodedIdToken.picture
+      };
+      req.user = authUser;
+      next();
+    }).catch(error => {
+      next();
+    });
+  } else {
+    next();  
+  };
+};
+
+app.use(checkUser);
